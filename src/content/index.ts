d30e087f0c1b05d97a033ad68655fde37b454291
mrfusiion <fuse-x-fuzionzz@outlook.be>
2023-08-11 08:39:28 +0200

    Initial content script

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,66 @@
-console.log("[FMG] content script loaded");
+import { timeout, waitForGlobals } from "@/shared/async";
+import { createScript, getElement } from "@shared/dom";
+import { getPageType } from "@shared/page";
+
+/*
+ * Because we delayed the map script, we need to manually create the google maps object.
+ * If altMapSdk is enabled.
+ * Because google.maps will allready be defined by another script,
+ * and there for the map script will never define a maps mock object.
+ **/
+function fixGoogleMaps(): void {
+    if (window.config?.altMapSdk) {
+        window.google = window.google || {};
+        window.google.maps = {
+            Size() {}
+        };
+    }
+}
+
+/*
+ * Load the map script, and wait for the globals to be defined.
+ **/
+async function loadMapScript(): Promise<void> {
+    const script = await getElement<HTMLScriptElement>(
+        "script[src^='https://cdn.mapgenie.io/js/map.js?id=']"
+    );
+
+    if (!script) throw new Error("Map script not found");
+
+    createScript({
+        src: script.src.replace("id=", "ready&id="),
+        appendTo: document.body
+    });
+
+    return timeout(waitForGlobals(["axios", "store"], window), 10 * 1000);
+}
+
+async function init() {
+    // If window.store is defined, the page has allready been loaded.
+    // This can happen if the extension is reloaded.
+    if (window.store) {
+        location.reload();
+        throw new Error("Store allready defined, reloading page");
+    }
+
+    // Fix google maps
+    fixGoogleMaps();
+
+    // Load the mapgenie map script, that was previously blocked by the background script.
+    await loadMapScript();
+
+    const type = getPageType(window);
+    switch (type) {
+        default:
+            console.warn(`[FMG] Page type ${type}, not installing map/guide!`);
+            return;
+    }
+}
+
+init()
+    .then(() => {
+        console.log("[FMG] content script init done");
+    })
+    .catch((err) => {
+        console.error("[FMG] Failed to get script", err);
+    });
