72e46d8c4291d61bfef9fafb874b31747d568e18
mrfusiion <fuse-x-fuzionzz@outlook.be>
2023-08-17 04:23:28 +0200

    Added comments

--- src/content/map-selector/index.ts
+++ src/content/map-selector/index.ts
@@ -11,6 +11,12 @@ export class FMG_MapSelector {
         this.window = window;
     }
 
+    /**
+     * Installs the map selector in the window.
+     * Will only install once.
+     * @param window the window to install the map selector in
+     * @returns the installed map selector
+     */
     public static install(window: WindowExtened): FMG_MapSelector {
         if (!window[FmgMapSelectorInstalled]) {
             window[FmgMapSelectorInstalled] = new FMG_MapSelector(window);
@@ -18,6 +24,12 @@ export class FMG_MapSelector {
         return window[FmgMapSelectorInstalled];
     }
 
+    /**
+     * Searches for a map that is free and returns the url.
+     * @param window the window to search in
+     * @returns the url of the first free map
+     * @throws if no free map is found
+     */
     private static getFreeFirstFreeMapUrl(window: Window): string {
         for (const item of window.document.querySelectorAll(".game-item")) {
             if (!item.classList.contains("unavailable")) {
@@ -27,6 +39,12 @@ export class FMG_MapSelector {
         throw new Error("No free maps found");
     }
 
+    /**
+     * Get the map name from the item.
+     * @param item the item to get the map name from
+     * @returns the map name
+     * @throws if the map name could not be found
+     */
     private static getMapName(item: Element): string {
         const img = item.querySelector<HTMLImageElement>("img");
         if (img?.alt) {
@@ -40,6 +58,11 @@ export class FMG_MapSelector {
         throw new Error("Could not find map name");
     }
 
+    /**
+     * Unlocks all maps locked behind pro paywall.
+     * @param window the window to unlock the maps in
+     * @throws if no free map is found or the map name could not be found for a specific map
+     */
     private static unlockProMaps(window: Window): void {
         const freeMapUrl = this.getFreeFirstFreeMapUrl(window);
         window.document.querySelectorAll(".game-item").forEach((item) => {
@@ -68,6 +91,10 @@ export class FMG_MapSelector {
         });
     }
 
+    /**
+     * Sets up the map selector.
+     * @param window the window to setup the map selector in
+     */
     public static async setup(window: Window): Promise<void> {
         this.unlockProMaps(window);
     }
