3fb078fb4fde0ab51552fcff378fbe2c2cfc80b9
mrfusiion <fuse-x-fuzionzz@outlook.be>
2023-08-21 11:04:33 +0200

    Categories are now saved and loaded

--- src/content/map/filters/api-filter/categories.ts
+++ src/content/map/filters/api-filter/categories.ts
@@ -1,4 +1,24 @@
 import type { FMG_ApiFilter } from "@fmg/filters/api-filter";
 import type { FMG_MapManager } from "@content/map/map-manager";
 
-export default function (filter: FMG_ApiFilter, mapManager: FMG_MapManager) {}
+export default function (filter: FMG_ApiFilter, mapManager: FMG_MapManager) {
+    filter.registerFilter<{ category: number }>(
+        "post",
+        "categories",
+        (_method, _key, _id, data, _url, block) => {
+            logger.debug("track category", data.category);
+            mapManager.storage.data.categories[data.category] = true;
+            block();
+        }
+    );
+
+    filter.registerFilter<undefined>(
+        "delete",
+        "categories",
+        (_method, _key, id, _data, _url, block) => {
+            logger.debug("untrack category", id);
+            delete mapManager.storage.data.categories[id];
+            block();
+        }
+    );
+}
