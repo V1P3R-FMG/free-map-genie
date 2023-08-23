ad806634e19b329cab03719d53ae8ad4bb959530
mrfusiion <fuse-x-fuzionzz@outlook.be>
2023-08-24 01:25:21 +0200

    Fix nan progress bar when no categories tracked

--- src/content/map/ui/progress.vue
+++ src/content/map/ui/progress.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import type { FMG_MapManager } from "@fmg/map-manager";
 
 const total = ref(0);
 const marked = ref(0);
+const percent = ref(100);
+
+watch([total, marked], () => {
+    if (total.value === 0 && marked.value === 0) {
+        percent.value = 100;
+        return;
+    }
+    percent.value = (marked.value / total.value) * 100;
+});
 
 function recalculate(mapManager: FMG_MapManager) {
     total.value = 0;
@@ -31,15 +40,13 @@ defineExpose({
 <template>
     <div class="progress-item-wrapper" style="margin-right: 10px">
         <div class="progress-item">
-            <span class="title">{{
-                +((marked / total) * 100).toFixed(2) + "%"
-            }}</span>
+            <span class="title">{{ +percent.toFixed(2) + "%" }}</span>
             <span class="counter">{{ marked }} / {{ total }}</span>
             <div class="progress-bar-container">
                 <div
                     class="progress-bar"
                     role="progressbar"
-                    :style="{ width: (marked / total) * 100 + '%' }"
+                    :style="{ width: percent + '%' }"
                 ></div>
             </div>
         </div>
