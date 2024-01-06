0cf66e7d40f190d8303c8042954291d40bc6ff4c
= <=>
2024-01-06 22:28:32 +0100

    Added timeout for get functions and added some waiter functions for dom

--- src/shared/dom.ts
+++ src/shared/dom.ts
@@ -1,31 +1,37 @@
-import { sleep, waitForCallback } from "./async";
+import { sleep, timeout, waitForCallback } from "./async";
 
-export async function getElement<T extends Element>(
+export function getElement<T extends Element>(
     selector: string,
-    winOrElement?: Window | Element
+    winOrElement?: Window | Element,
+    timeoutTime: number = -1
 ): Promise<T> {
-    const parent = winOrElement instanceof Element ? winOrElement : winOrElement?.document ?? window.document;
-    let element =parent.querySelector(selector);
-    if (element !== null) return element as T;
-    while (element === null) {
-        await sleep(100);
-        element = parent.querySelector(selector);
-    }
-    return element as T;
+    return timeout((async () => {
+        const parent = winOrElement instanceof Window ? winOrElement.document : winOrElement ?? window.document;
+        let element = parent.querySelector(selector);
+        if (element !== null) return element as T;
+        while (element === null) {
+            await sleep(100);
+            element = parent.querySelector(selector);
+        }
+        return element as T;
+    })(), timeoutTime, `Failed to get element ${selector}.`);
 }
 
-export async function getElementWithXPath<T extends Element>(
+export function getElementWithXPath<T extends Element>(
     xpath: string,
-    win?: Window
+    win?: Window,
+    timeoutTime: number = -1
 ): Promise<T> {
-    win = win ?? window;
-    let element = win.document.evaluate(xpath, win.document).iterateNext();
-    if (element !== null) return element as T;
-    while (element === null) {
-        await sleep(100);
-        element = win.document.evaluate(xpath, win.document).iterateNext();
-    }
-    return element as T;
+    return timeout((async () => {
+        win = win ?? window;
+        let element = win.document.evaluate(xpath, win.document).iterateNext();
+        if (element !== null) return element as T;
+        while (element === null) {
+            await sleep(100);
+            element = win.document.evaluate(xpath, win.document).iterateNext();
+        }
+        return element as T;
+    })(), timeoutTime, `Failed to get element ${xpath}.`);
 }
 
 export interface CreateScriptOptions {
@@ -35,6 +41,14 @@ export interface CreateScriptOptions {
     appendTo?: HTMLElement;
 }
 
-export function documentLoaded(window: Window): Promise<void> {
-    return waitForCallback(() => window.document.readyState === "complete");
+export function documentLoaded(win?: Window): Promise<void> {
+    return waitForCallback(() => (win ?? window).document.readyState === "complete");
+}
+
+export async function waitForBody(win: Window): Promise<void> {
+    await waitForCallback(() => !!(win ?? window).document.body);
+}
+
+export async function waitForHead(win: Window): Promise<void> {
+    await waitForCallback(() => !!(win ?? window).document.head);
 }
