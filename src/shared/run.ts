be41da82894fca2171ab5699258f8a8d1b61b081
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-07-25 15:32:54 +0200

    Add utils to run an async context with result logging

---
+++ src/shared/run.ts
@@ -0,0 +1,21 @@
+export interface RunContext {
+    (): Promise<any> | any;
+}
+
+async function runContext(context: RunContext): Promise<any> {
+    return await context();
+}
+
+export default async function runContexts(
+    name: string,
+    ...contexts: RunContext[]
+): Promise<boolean> {
+    let success = true;
+    await Promise.all(contexts.map(runContext))
+        .then(() => logger.log(`FMG ${name} context initialized.`))
+        .catch((e) => {
+            success = false;
+            logger.error(`Failed to initalize ${name} context reason,`, e);
+        });
+    return success;
+}
