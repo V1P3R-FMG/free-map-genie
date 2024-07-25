ea0ee7ac3a2702f2d06085fcc204b220ec59c207
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-07-25 15:13:27 +0200

    Add inject utils

---
+++ src/shared/inject.ts
@@ -0,0 +1,25 @@
+export function injectScript(src: string, query?: Record<string, string>) {
+    const url = new URL(src);
+    if (query) {
+        Object.entries(query).forEach(([key, value]) =>
+            url.searchParams.set(key, value)
+        );
+    }
+    return new Promise(async (resolve) => {
+        const script = document.createElement("script");
+        script.src = url.toString();
+        script.onload = resolve;
+        script.onerror = resolve;
+
+        await document
+            .waitForDocumentHead()
+            .then((head) => head.appendChild(script));
+    });
+}
+
+export function injectExtensionScript(
+    path: string,
+    query?: Record<string, string>
+) {
+    return injectScript(chrome.runtime.getURL(path), query);
+}
