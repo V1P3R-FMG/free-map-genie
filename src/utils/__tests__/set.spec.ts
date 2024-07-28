2135121c6d23bccbfbe5f1875109a4b7f2dbe675
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-07-29 01:32:28 +0200

    added set util

---
+++ src/utils/__tests__/set.spec.ts
@@ -0,0 +1,223 @@
+import { expect, test } from "@jest/globals";
+
+import NumberSet from "@utils/set.js";
+
+const stringArray = (...arr: number[]) => arr.map(String);
+const numberArray = (...arr: number[]) => arr;
+
+describe.each([
+    ["string", stringArray],
+    ["number", numberArray],
+])("NumberSet with %s values", (_, defineArray) => {
+    describe("initialization", () => {
+        /** NumberSet.new */
+        it("should be creatable from an array", () => {
+            const set = NumberSet.new(defineArray(1, 2, 3));
+            expect(set.values()).toEqual([1, 2, 3]);
+        });
+    });
+
+    describe("operations", () => {
+        /** NumberSet.size */
+        test.each([
+            [1, defineArray(5)],
+            [6, defineArray(5, 6, 7, 1, 2, 3)],
+            [3, defineArray(20, 7, 6)],
+        ])("should give the correct size for an array with length %i", (length, array) => {
+            const set = NumberSet.new(array);
+            expect(set.size).toBe(length);
+        });
+
+        /** NumberSet.values() */
+        test.each([
+            [defineArray(1, 2, 3), [1, 2, 3]],
+            [defineArray(3, 5, 20, 3), [3, 5, 20]],
+        ])("should give the correct values %p", (array, expected) => {
+            const set = NumberSet.new(array);
+            expect(set.values()).toEqual(expected);
+        });
+
+        /** NumberSet.toggle(i) */
+        test.each([[defineArray(1, 2, 3, 6), [3, 4, 6], [1, 2, 4]]])(
+            "should NumberSet.toggle values",
+            (array, indexes, expected) => {
+                const set = NumberSet.new(array);
+
+                for (const index of indexes) {
+                    set.toggle(index);
+                }
+
+                expect(set.values()).toEqual(expected);
+            }
+        );
+
+        /** NumberSet.toggle(i, true) */
+        test.each([[defineArray(1, 2, 3, 6), [3, 4, 6], [1, 2, 3, 4, 6]]])(
+            "should add values when NumberSet.toggle is called with true",
+            (array, indexes, expected) => {
+                const set = NumberSet.new(array);
+
+                for (const index of indexes) {
+                    set.toggle(index, true);
+                }
+
+                expect(set.values().sort()).toEqual(expected.sort());
+            }
+        );
+
+        /** NumberSet.toggle(i, false) */
+        test.each([[defineArray(1, 2, 3, 6), [3, 4, 6], [1, 2]]])(
+            "should remove values when NumberSet.toggle is called with false",
+            (array, indexes, expected) => {
+                const set = NumberSet.new(array);
+
+                for (const index of indexes) {
+                    set.toggle(index, false);
+                }
+
+                expect(set.values().sort()).toEqual(expected.sort());
+            }
+        );
+
+        /** NumberSet.add(i) */
+        test.each([[defineArray(1, 2, 3), 3]])("should add values", (indexes, length) => {
+            const set = NumberSet.new();
+
+            for (const index of indexes) {
+                set.add(index);
+            }
+
+            expect(set.size).toBe(length);
+        });
+
+        /** NumberSet.delete(i) */
+        test.each([[defineArray(1, 2, 3)]])("should remove values", (indexes) => {
+            const set = NumberSet.new(indexes);
+
+            for (const index of indexes) {
+                set.delete(index);
+            }
+
+            expect(set.size).toBe(0);
+        });
+
+        /** NumberSet.has(i) */
+        test.each([
+            [
+                defineArray(1, 3),
+                [
+                    [1, true] as [number, boolean],
+                    [2, false] as [number, boolean],
+                    [3, true] as [number, boolean],
+                    [4, false] as [number, boolean],
+                ],
+            ],
+        ])(
+            "should give if exists true or false when it doesn't exist when NumberSet.has is called",
+            (array, expected) => {
+                const set = NumberSet.new(array);
+
+                for (const [index, bool] of expected) {
+                    expect(set.has(index)).toEqual(bool);
+                }
+            }
+        );
+    });
+
+    describe("indexing", () => {
+        /** NumberSet[i] */
+        test.each([[defineArray(1, 2, 3), [1, 2, 3]]])("should be indexable", (array, indexes) => {
+            const set = NumberSet.new(array);
+
+            for (const index of indexes) {
+                expect(set[index]).toBe(true);
+            }
+        });
+
+        /** NumberSet[i] = true */
+        test.each([[defineArray(1, 2, 3), 3]])("should add values via index", (indexes, length) => {
+            const set = NumberSet.new();
+
+            for (const index of indexes) {
+                set[index] = true;
+                expect(set[index]).toBe(true);
+            }
+
+            expect(set.size).toEqual(length);
+        });
+
+        /** NumberSet[i] = false */
+        test.each([[defineArray(1, 2, 3)]])("should remove values via index", (indexes) => {
+            const set = NumberSet.new(indexes);
+
+            for (const index of indexes) {
+                set[index] = false;
+                expect(set[index]).toBe(false);
+            }
+
+            expect(set.size).toEqual(0);
+        });
+
+        /** NumberSet[i] = any */
+        it("should error when setting a value other than a boolean", () => {
+            const set = NumberSet.new();
+
+            expect(() => (set[1] = "1" as any)).toThrow("Expected boolen value");
+            expect(() => (set[1] = 50 as any)).toThrow("Expected boolen value");
+            expect(() => (set[1] = {} as any)).toThrow("Expected boolen value");
+            expect(() => (set[1] = [] as any)).toThrow("Expected boolen value");
+        });
+    });
+
+    describe("iterating", () => {
+        it("should be iterable with in", () => {
+            const array = [6, 20, 3, 76];
+
+            const set = NumberSet.new(defineArray(...array));
+
+            const iter = array.values();
+
+            for (const index in set) {
+                expect(index).toBe(String(iter.next().value));
+            }
+        });
+
+        it("should work with Object.keys()", () => {
+            const array = [6, 20, 3, 76];
+
+            const set = NumberSet.new(defineArray(...array));
+
+            expect(Object.keys(set).sort()).toEqual(array.map(String).sort());
+        });
+
+        it("should work with Object.values()", () => {
+            const array = [6, 20, 3, 76];
+
+            const set = NumberSet.new(defineArray(...array));
+
+            expect(Object.values(set)).toEqual(array.map(() => true));
+        });
+
+        it("should work with Object.entries()", () => {
+            const array = [6, 20, 3, 76];
+
+            const set = NumberSet.new(defineArray(...array));
+
+            expect(Object.entries(set).sort()).toEqual(array.map((i) => [String(i), true]).sort());
+        });
+    });
+});
+
+describe("NumberSet statics", () => {
+    /** NumberSet.isIndexableSet */
+    it("should give true for NumberSet's and false for any oter value when NumberSet.isIndexableSet is called", () => {
+        expect(NumberSet.isIndexableSet([1, 2, 3])).toBe(false);
+        expect(NumberSet.isIndexableSet(["1", "2", "3"])).toBe(false);
+        expect(NumberSet.isIndexableSet(1)).toBe(false);
+        expect(NumberSet.isIndexableSet("1")).toBe(false);
+        expect(NumberSet.isIndexableSet({ 1: true })).toBe(false);
+
+        expect(NumberSet.isIndexableSet(NumberSet.new([1, 2, 3]))).toBe(true);
+        expect(NumberSet.isIndexableSet(NumberSet.new(["1", "2", "3"]))).toBe(true);
+    });
+});
---
+++ src/utils/set.ts
@@ -0,0 +1,110 @@
+export type NumberSetLayout = number[];
+
+export interface Indexable {
+    [value: number]: boolean;
+    [value: string]: boolean;
+}
+
+export type IndexableNumberSet = NumberSet & Indexable;
+
+export class IndexableSetProxyHandler implements ProxyHandler<NumberSet> {
+    public get(target: NumberSet, p: string | symbol, recv: any) {
+        if (typeof p === "symbol" || Reflect.has(target, p)) {
+            return Reflect.get(target, p, recv);
+        } else {
+            return target.has.call(target, p);
+        }
+    }
+
+    public set(target: NumberSet, p: string | symbol, value: any) {
+        if (typeof p === "symbol" || Reflect.has(target, p)) {
+            throw "Cannot override NumberSet methods.";
+        } else {
+            if (typeof value !== "boolean") {
+                throw "Expected boolen value";
+            }
+            target.toggle.call(target, p, value);
+            return true;
+        }
+    }
+
+    public has(target: NumberSet, p: string | symbol) {
+        if (typeof p === "symbol" || Reflect.has(target, p)) {
+            return Reflect.has(target, p);
+        } else {
+            return target.has.call(target, p);
+        }
+    }
+
+    public deleteProperty(target: NumberSet, p: string | symbol): boolean {
+        if (typeof p === "symbol" || Reflect.has(target, p)) {
+            return Reflect.deleteProperty(target, p);
+        } else {
+            return target.delete.call(target, p);
+        }
+    }
+
+    public ownKeys(target: NumberSet): ArrayLike<string> {
+        return target.values.call(target).map(String);
+    }
+
+    public getOwnPropertyDescriptor(target: NumberSet, p: string | symbol): PropertyDescriptor | undefined {
+        if (typeof p === "symbol" || Reflect.has(target, p)) {
+            return Reflect.getOwnPropertyDescriptor(target, p);
+        } else {
+            return {
+                value: target.has.call(target, p),
+                enumerable: true,
+                configurable: true,
+            };
+        }
+    }
+}
+
+export default class NumberSet {
+    private readonly internal: Set<number> = new Set();
+
+    private constructor(iterable: Iterable<number>) {
+        this.internal = new Set(iterable);
+    }
+
+    public static isIndexableSet(obj: any): obj is IndexableNumberSet {
+        return obj instanceof NumberSet;
+    }
+
+    public static new(array?: Array<number | string>): IndexableNumberSet {
+        return new Proxy(new NumberSet(array?.map(Number) ?? []), new IndexableSetProxyHandler()) as IndexableNumberSet;
+    }
+
+    public toggle(value: number | string, toggled?: boolean): boolean {
+        value = Number(value);
+        toggled ??= !this.internal.has(value);
+        if (toggled) {
+            this.internal.add(value);
+            return true;
+        } else {
+            this.internal.delete(value);
+            return false;
+        }
+    }
+
+    public has(value: number | string): boolean {
+        return this.internal.has(Number(value));
+    }
+
+    public add(value: number | string) {
+        this.internal.add(Number(value));
+    }
+
+    public delete(value: number | string): boolean {
+        return this.internal.delete(Number(value));
+    }
+
+    public values(): NumberSetLayout {
+        return [...this.internal.values()];
+    }
+
+    public get size(): number {
+        return this.internal.size;
+    }
+}
