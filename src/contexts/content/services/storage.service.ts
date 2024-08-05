04d9049a44ed735061c06ad411f83a213946179f
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-08-05 02:51:31 +0200

    added storage.service

---
+++ src/contexts/content/services/storage.service.ts
@@ -0,0 +1,67 @@
+import DataManager, { type LatestData } from "@content/storage/data/index";
+
+import WindowStorageDriver from "@content/storage/drivers/window.driver";
+import IframeStorageDriver from "@content/storage/drivers/iframe.driver";
+
+export interface Updater {
+    (data: LatestData): Promise<any> | any;
+}
+
+class StorageService {
+    private readonly localStorage = new DataManager(new WindowStorageDriver());
+    private readonly iframeStorage = new DataManager(new IframeStorageDriver());
+
+    private readonly cache: Record<string, LatestData> = {};
+
+    /** Migrates data from localstorage to iframestorage  */
+    private async migrate(key: Key) {
+        // We don't need to migrate as the iframe storage is located at this domain
+        if (window.location.origin === "https://mapgenie.io") return;
+
+        // We don't want to accidentally override current data with old data.
+        if (await this.iframeStorage.has(key)) return;
+
+        if (await this.localStorage.has(key)) {
+            const data = await this.localStorage.load(key);
+            await this.iframeStorage.save(key, data);
+            await this.localStorage.backup(
+                this.localStorage.current.version,
+                this.localStorage.current.backupKeys(key)
+            );
+            await this.localStorage.remove(key);
+        }
+    }
+
+    public loadFromCache(key: Key): LatestData | undefined {
+        return this.cache[`${key}`];
+    }
+
+    public async load(key: Key) {
+        if (!this.cache[`${key}`]) {
+            await this.migrate(key);
+            this.cache[`${key}`] = await this.iframeStorage.load(key);
+        }
+        return this.cache[`${key}`];
+    }
+
+    public async save(key: Key) {
+        if (!this.cache[`${key}`]) {
+            logger.warn("Tried to save data that is not loaded.", key);
+            return;
+        }
+        await this.iframeStorage.save(key, this.cache[`${key}`]);
+    }
+
+    public async remove(key: Key) {
+        delete this.cache[`${key}`];
+        await this.iframeStorage.remove(key);
+    }
+
+    public async update(key: Key, updater: Updater) {
+        const data = await this.load(key);
+        await updater(data);
+        await this.save(key);
+    }
+}
+
+export default new StorageService();
