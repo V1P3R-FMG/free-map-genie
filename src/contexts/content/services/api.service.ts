cfdb1d8c6d5191ff60518caf79f5f2e3a6ee6e8b
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-08-05 02:51:44 +0200

    added api.service

---
+++ src/contexts/content/services/api.service.ts
@@ -0,0 +1,33 @@
+import ApiFilter from "@fmg/filters/api-filter";
+import Key from "@content/storage/key";
+
+import storageService from "@content/services/storage.service";
+
+class ApiService {
+    public async install() {
+        const filter = await ApiFilter.install(window);
+
+        filter.registerFilter(["put", "delete"], { path: "locations", hasId: true }, async ({ id, method }) => {
+            await storageService.update(Key.fromWindow(window), (data) => {
+                data.locations.toggle(id!, method === "put");
+            });
+            return { block: true };
+        });
+
+        filter.registerFilter<{ category: number }>("post", { path: "categories" }, async ({ postData }) => {
+            await storageService.update(Key.fromWindow(window), (data) => {
+                data.categories.add(postData.category);
+            });
+            return { block: true };
+        });
+
+        filter.registerFilter("delete", { path: "categories", hasId: true }, async ({ id }) => {
+            await storageService.update(Key.fromWindow(window), (data) => {
+                data.categories.delete(id!);
+            });
+            return { block: true };
+        });
+    }
+}
+
+export default new ApiService();
