42b07e37b4bff9272a8cef3bf1eb84739e83166a
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-08-09 17:24:37 +0200

    added vue

---
+++ webpack/config.ts
@@ -0,0 +1,157 @@
+import path from "node:path";
+import webpack from "webpack";
+import "dotenv/config";
+
+import getBuildInfo from "./buildInfo.js";
+import WebExtManifestPlugin from "./plugins/web-ext-manifest.js";
+
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import TerserPlugin from "terser-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { VueLoaderPlugin } from "vue-loader";
+
+const { ProvidePlugin, DefinePlugin } = webpack;
+
+const __dirname = import.meta.dirname;
+
+export const PORT = process.env.PORT ? Number(process.env.PORT) : 8080;
+export const CACHE_MAX_AGE = process.env.PORT ? Number(process.env.PORT) : 30 * 60 * 1000;
+export const MAX_BACKUPS_COUNT = process.env.MAX_BACKUPS_COUNT ? Number(process.env.MAX_BACKUPS_COUNT) : 10;
+
+export default async function getConfig(): Promise<webpack.Configuration> {
+    const buildInfo = getBuildInfo(path.resolve(__dirname, "..", "build"));
+    const { default: WebExtPlugin } = await import("web-ext-plugin");
+
+    return {
+        entry: {
+            extension: "./src/contexts/extension/index.ts",
+            background: "./src/contexts/background/index.ts",
+            content: "./src/contexts/content/index.ts",
+            iframe: "./src/contexts/iframe/index.ts",
+        },
+        output: { path: buildInfo.out },
+        resolve: {
+            extensions: [".ts", ".js", ".json", ".vue"],
+            alias: {
+                vue$: buildInfo.isDev
+                    ? "vue/dist/vue.runtime.esm-browser.js"
+                    : "vue/dist/vue.runtime.esm-browser.prod.js",
+            },
+            plugins: [
+                new TsconfigPathsPlugin({
+                    configFile: path.resolve(__dirname, "..", "tsconfig.json"),
+                }),
+            ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /(node_modules)/,
+                    use: [
+                        "gnirts-loader",
+                        path.resolve(__dirname, "loaders", "inject-globals.js"),
+                        {
+                            loader: "swc-loader",
+                            options: {
+                                sourceMaps: buildInfo.isDev,
+                                jsc: {
+                                    parser: {
+                                        syntax: "typescript",
+                                    },
+                                    target: "es2020",
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: {
+                                url: false,
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "css/[name].css",
+            }),
+            new VueLoaderPlugin(),
+            new CopyPlugin({ patterns: [{ context: "src", from: "assets", to: "assets" }] }),
+            new DefinePlugin({
+                __BROWSER__: JSON.stringify(buildInfo.browser),
+                __VERSION__: JSON.stringify(buildInfo.version),
+                __HOMEPAGE__: JSON.stringify(buildInfo.homepage),
+                __AUTHOR__: JSON.stringify(buildInfo.author),
+                __DEBUG__: buildInfo.isDev,
+                __WATCH__: buildInfo.watch,
+                __PORT__: PORT,
+                __CACHE_MAX_AGE__: CACHE_MAX_AGE,
+                __MAX_BACKUPS_COUNT__: MAX_BACKUPS_COUNT,
+                //console: "logging",
+            }),
+            new ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                logging: [path.resolve(__dirname, "..", "src", "logging.ts"), "default"],
+            }),
+            new WebExtManifestPlugin({
+                files: [
+                    "./src/manifest.json",
+                    `./src/manifest.${buildInfo.browser}.json`,
+                    {
+                        key: buildInfo.keys.public,
+                        update_url: buildInfo.updateUrl,
+                    },
+                ],
+                fields: ["version", "author", "name"],
+                tabs: 2,
+            }),
+            new WebExtPlugin({
+                artifactsDir: buildInfo.out,
+                target: buildInfo.isChrome ? "chromium" : "firefox-desktop",
+                buildPackage: !buildInfo.watch && !buildInfo.isDev,
+                outputFilename: buildInfo.isDev
+                    ? buildInfo.name
+                    : buildInfo.name + (buildInfo.isChrome ? ".zip" : ".xpi"),
+                runLint: false,
+                selfHosted: true,
+                overwriteDest: true,
+                devtools: true,
+                startUrl: process.env.START_URL ?? "https://mapgenie.io",
+                chromiumBinary: process.env.CHROME_BIN,
+                sourceDir: buildInfo.out,
+                args: buildInfo.isChrome
+                    ? ["--auto-open-devtools-for-tabs", "--system-developer-mode", "--start-maximized"]
+                    : [],
+            }),
+        ],
+        optimization: {
+            splitChunks: {
+                filename: "chunks/[name].js",
+            },
+            minimize: !buildInfo.isDev,
+            minimizer: [new TerserPlugin()],
+        },
+        watch: buildInfo.watch,
+        devtool: buildInfo.isDev ? "inline-cheap-module-source-map" : false,
+        watchOptions: {
+            ignored: "node_modules",
+            poll: 1000,
+            aggregateTimeout: 300,
+        },
+        mode: buildInfo.mode,
+    };
+}
