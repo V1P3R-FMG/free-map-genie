fee7bda0b97e13822f578d29b6b20e2599080d04
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-09-16 08:21:48 +0200

    renamed prop from 'extension' to 'channel'

--- src/contexts/content/channels/base.channel.ts
+++ src/contexts/content/channels/base.channel.ts
@@ -2,23 +2,23 @@ import Channel from "@shared/channel";
 import { Channels } from "@constants";
 
 import type { MessageScheme as BackgroundMessageScheme } from "@background/index";
-import type { MessageScheme as ExtensionMessageScheme } from "@extension/index";
+import type { ExtensionMessageScheme } from "@extension/index";
 import type { MessageScheme as IframeMessageScheme } from "contexts/storage/main";
 
 export default class BaseChannel {
-    private readonly extension = Channel.window(Channels.Content);
+    private readonly channel = Channel.window(Channels.Content);
 
     protected sendBackground(message: BackgroundMessageScheme, timeout?: number) {
         // Still sending to extension but it will forward this message
-        return this.extension.send(Channels.Extension, message, timeout);
+        return this.channel.send(Channels.Extension, message, timeout);
     }
 
     protected sendExtension(message: ExtensionMessageScheme, timeout?: number) {
-        return this.extension.send(Channels.Extension, message, timeout);
+        return this.channel.send(Channels.Extension, message, timeout);
     }
 
     protected sendIframe(message: IframeMessageScheme, timeout?: number) {
         // Still sending to extension but it will forward this message
-        return this.extension.send(Channels.Extension, message, timeout);
+        return this.channel.send(Channels.Extension, message, timeout);
     }
 }
