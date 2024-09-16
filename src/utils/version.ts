2316f0a7d51574fc3e0f5f2a5e86902c96a251bc
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-09-16 08:18:19 +0200

    added version util functions

---
+++ src/utils/version.ts
@@ -0,0 +1,23 @@
+export function full(version: string): string {
+    const parts = version.split(".").slice(0, 4);
+    while (parts.length < 4) {
+        parts.push("0");
+    }
+    return parts.join(".");
+}
+
+export function compare(versionA: string, versionB: string): number {
+    const versionAParts = versionA.split(".");
+    const versionBParts = versionB.split(".");
+
+    const maxLength = Math.max(versionAParts.length, versionBParts.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        const aPart = versionAParts[i] || "0";
+        const bPart = versionBParts[i] || "0";
+        if (aPart > bPart) return 1;
+        if (aPart < bPart) return -1;
+    }
+
+    return 0;
+}
