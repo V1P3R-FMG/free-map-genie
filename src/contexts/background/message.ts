5e0fcfc37e35ba442c1be655bf6b65e08951345e
Mrfusiion <fuse-x-fuzionzz@outlook.be>
2024-09-16 08:22:12 +0200

    cleanup message handling for background

--- src/contexts/background/message.ts
+++ src/contexts/background/message.ts
@@ -1,17 +1,41 @@
 import { Channels } from "@constants";
 import Channel, { type ChannelMessage } from "@shared/channel";
 
-export function forwardMessage(sender: chrome.runtime.MessageSender, message: ChannelMessage): boolean {
-    const tabId = message.tabId || sender.tab?.id;
-    if (message.target === Channels.Mapgenie) {
-        //chrome.runtime.sendMessage(message);
-        return false;
-    } else if (tabId) {
+function sendMessageToTab(tabId: number, sender: chrome.runtime.MessageSender, message: ChannelMessage) {
+    chrome.tabs.sendMessage(tabId, {
+        origin: sender.origin,
+        data: message,
+    });
+    return true;
+}
+
+async function getActiveTabId() {
+    const tabs = await chrome.tabs.query({ active: true });
+    return tabs[0]?.id;
+}
+
+async function sendMessageToActiveTab(sender: chrome.runtime.MessageSender, message: ChannelMessage) {
+    const tabId = await getActiveTabId();
+    if (typeof tabId === "number") {
         chrome.tabs.sendMessage(tabId, {
             origin: sender.origin,
             data: message,
         });
         return true;
+    } else {
+        logging.warn("Failed to send message from popup active tab.id not found", sender);
+        return false;
+    }
+}
+
+export async function forwardMessage(sender: chrome.runtime.MessageSender, message: ChannelMessage): Promise<boolean> {
+    const tabId = message.tabId || sender.tab?.id;
+    if (message.target === Channels.Mapgenie) {
+        return false;
+    } else if (tabId) {
+        return sendMessageToTab(tabId, sender, message);
+    } else if (sender.url?.endsWith("popup/index.html")) {
+        return sendMessageToActiveTab(sender, message);
     } else {
         logging.warn("Unable to forward message sender has no tab.id", sender);
         return false;
