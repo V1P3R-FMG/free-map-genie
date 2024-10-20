63790e24e7da559983e290f5508cc8c169494a5e
MrFusiion <fuse-x-fuzionzz@outlook.be>
2024-10-20 11:05:51 +0200

    push rules into array and auto extract ids when updating dynamic rules

--- src/contexts/background/rules.ts
+++ src/contexts/background/rules.ts
@@ -15,39 +15,44 @@ function headerOperation(operation: `${HeaderOperation}`): HeaderOperation {
 }
 
 export default async function installRules() {
+    const rules: chrome.declarativeNetRequest.Rule[] = [];
+
+    // Allow https://mapgenie.io to be loaded as an iframe on other domains.
+    rules.push({
+        id: 1,
+        priority: 1,
+        action: {
+            type: ruleActionType("modifyHeaders"),
+            responseHeaders: ["X-Frame-Options", "Frame-Options"].map((header) => ({
+                header,
+                operation: headerOperation("remove"),
+            })),
+        },
+        condition: {
+            requestDomains: ["mapgenie.io"],
+            resourceTypes: __DEBUG__
+                ? [resourceType("sub_frame"), resourceType("main_frame")]
+                : [resourceType("sub_frame")],
+        },
+    });
+
+    // Block map.js?id=123456abcdef but allow map.js?ready&id=123456abcdef
+    // We block the script so the extension has time setup.
+    // The extension will reinject the script but with ready in the search query after the setup.
+    rules.push({
+        id: 2,
+        priority: 1,
+        action: {
+            type: ruleActionType("block"),
+        },
+        condition: {
+            regexFilter: "^https://cdn\\.mapgenie\\.io/js/map\\.js\\?id=\\w+$",
+            resourceTypes: [resourceType("script")],
+        },
+    });
+
     await chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [1, 2],
-        addRules: [
-            // Allow https://mapgenie.io to be loaded as an iframe on other domains.
-            {
-                id: 1,
-                priority: 1,
-                action: {
-                    type: ruleActionType("modifyHeaders"),
-                    responseHeaders: ["X-Frame-Options", "Frame-Options"].map((header) => ({
-                        header,
-                        operation: headerOperation("remove"),
-                    })),
-                },
-                condition: {
-                    requestDomains: ["mapgenie.io"],
-                    resourceTypes: [resourceType("sub_frame")],
-                },
-            },
-            // Block map.js?id=123456abcdef but allow map.js?ready&id=123456abcdef
-            // We block the script so the extension has time setup.
-            // The extension will reinject the script but with ready in the search query after the setup.
-            {
-                id: 2,
-                priority: 1,
-                action: {
-                    type: ruleActionType("block"),
-                },
-                condition: {
-                    regexFilter: "^https://cdn\\.mapgenie\\.io/js/map\\.js\\?id=\\w+$",
-                    resourceTypes: [resourceType("script")],
-                },
-            },
-        ],
+        removeRuleIds: rules.map((rule) => rule.id),
+        addRules: rules,
     });
 }
