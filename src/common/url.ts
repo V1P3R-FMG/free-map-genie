54690a15dbf45dc3cb744e5984ddbed319ff60bc
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-07 17:46:50 +0100

    simplify normelizeurl function

--- src/common/url.ts
+++ src/common/url.ts
@@ -1,11 +1,3 @@
-export const toUrl = (url: string) => {
-  try {
-    return new URL(url, window.location.origin);
-  } catch {
-    return new URL("https://" + url, window.location.origin);
-  }
-};
-
 export const normalizeUrl = (url: string) => {
-  return toUrl(url).toString();
+  return new URL(url).toString();
 };
