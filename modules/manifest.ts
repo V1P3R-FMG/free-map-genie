44aeffd2bccb04cf59d59fa1d37e30af32feaff5
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-07 13:35:58 +0100

    update manifest wxt module to auto add an entry for the given background page entry

--- modules/manifest.ts
+++ modules/manifest.ts
@@ -1,20 +1,54 @@
+import path from "node:path";
 import { defineWxtModule } from "wxt/modules";
+
+import type { Wxt } from "wxt";
 
 export default defineWxtModule({
   setup(wxt) {
+    const generateEntrypointName = (file: string) => {
+      return file.replaceAll(/[\/\\]/g, "-").replace(/\.[^/.]+$/, "");
+    };
+
     wxt.hook("build:manifestGenerated", (wxt, manifest) => {
-      if ("background_page" in manifest) {
-        if (
-          wxt.config.browser === "firefox" ||
-          wxt.config.manifestVersion === 2
-        ) {
-          manifest.background = {
-            page: manifest.background_page,
-          };
-        }
+      if (!manifest.background_page) return;
+
+      const name = generateEntrypointName(manifest.background_page);
+
+      const isFirefox = wxt.config.browser === "firefox";
+      if (isFirefox) {
+        manifest.background = {
+          page: name + ".html",
+        };
       }
 
       delete manifest.background_page;
+    });
+
+    wxt.hook("entrypoints:resolved", (wxt, entrypoints) => {
+      if (!wxt.config.manifest.background_page) return;
+
+      const isFirefox = wxt.config.browser === "firefox";
+      if (!isFirefox) return;
+
+      const name = generateEntrypointName(wxt.config.manifest.background_page);
+
+      const entrypointsDir = path.resolve(
+        wxt.config.srcDir,
+        wxt.config.entrypointsDir
+      );
+
+      entrypoints.push({
+        name,
+        outputDir: wxt.config.outDir,
+        inputPath: path.resolve(
+          entrypointsDir,
+          wxt.config.manifest.background_page
+        ),
+        type: "unlisted-page",
+        options: {},
+      });
+
+      console.log("Resolved entrypoints:", entrypoints.length);
     });
   },
 });
