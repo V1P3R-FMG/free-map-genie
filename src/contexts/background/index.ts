a744090bad9d0bf9396ee0648c356b670936e5ba
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-07 17:46:22 +0100

    apply dnr rules in background script

--- src/contexts/background/index.ts
+++ src/contexts/background/index.ts
@@ -1,12 +1,14 @@
 import "@/common/messaging/contexts/background";
 
 import { createOffscreenDocument } from "./offscreen";
+import { addRules } from "./rules";
 
 import testService from "@/services/test.service";
 
 export default defineBackground(async () => {
   testService.provide();
 
+  await addRules();
   await createOffscreenDocument();
 
   logger.log("Hello background!", { id: browser.runtime.id });
