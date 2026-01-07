53d917a039f85155bc7beb0d666a610bbfeb41fb
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-07 14:11:09 +0100

    removed old background script

--- src/contexts/background.ts
+++
@@ -1,9 +0,0 @@
-import "@/common/messaging/contexts/background";
-
-import testService from "@/services/test.service";
-
-export default defineBackground(() => {
-  testService.provide();
-
-  console.log("Hello background!", { id: browser.runtime.id });
-});
