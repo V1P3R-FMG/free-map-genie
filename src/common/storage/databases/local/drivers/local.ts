cd382ef3ef71382566de36a92e2e3a3d2a697f07
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-09 16:20:18 +0100

    refactor localstoragedriver to use createasyncproxy when creating a storage service

--- src/common/storage/databases/local/drivers/local.ts
+++ src/common/storage/databases/local/drivers/local.ts
@@ -1,62 +1,53 @@
-import storageService from "../../../../../services/storage.service";
+import storageService from "@/services/storage.service";
+import { createAsyncProxy } from "@/common/asyncProxy";
+
 import type { Driver } from "./driver";
+import type { AsyncProxy } from "@/common/asyncProxy";
 
 export class LocalStorageDriver implements Driver {
-  private _storage?: storageService.Instance;
+  private storage: AsyncProxy<storageService.Instance>;
 
-  public constructor(private readonly domain: string) {}
-
-  private async getStorage() {
-    return (this._storage ??= await storageService.use(this.domain));
+  public constructor(domain: string) {
+    this.storage = createAsyncProxy(() => storageService.use(domain));
   }
 
   public async get(key: string): Promise<string | null> {
-    const storage = await this.getStorage();
-    return storage.get(key);
+    return this.storage.get(key);
   }
 
   public async getBulk(keys: string[]): Promise<Record<string, string | null>> {
-    const storage = await this.getStorage();
-    return storage.getBulk(keys);
+    return this.storage.getBulk(keys);
   }
 
   public async set(key: string, value: string): Promise<void> {
-    const storage = await this.getStorage();
-    return storage.set(key, value);
+    return this.storage.set(key, value);
   }
 
   public async setBulk(map: Record<string, string>): Promise<void> {
-    const storage = await this.getStorage();
-    return storage.setBulk(map);
+    return this.storage.setBulk(map);
   }
 
   public async remove(key: string): Promise<void> {
-    const storage = await this.getStorage();
-    return storage.remove(key);
+    return this.storage.remove(key);
   }
 
   public async removeBulk(keys: string[]): Promise<void> {
-    const storage = await this.getStorage();
-    return storage.removeBulk(keys);
+    return this.storage.removeBulk(keys);
   }
 
   public async has(key: string): Promise<boolean> {
-    const storage = await this.getStorage();
-    return storage.has(key);
+    return this.storage.has(key);
   }
 
   public async hasBulk(keys: string[]): Promise<Record<string, boolean>> {
-    const storage = await this.getStorage();
-    return storage.hasBulk(keys);
+    return this.storage.hasBulk(keys);
   }
 
   public async hasAny(keys: string[]): Promise<boolean> {
-    const storage = await this.getStorage();
-    return storage.hasAny(keys);
+    return this.storage.hasAny(keys);
   }
 
   public async keys(): Promise<string[]> {
-    const storage = await this.getStorage();
-    return storage.keys();
+    return this.storage.keys();
   }
 }
