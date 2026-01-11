6652796d372c174bdc2ee235c0346df7a64f3b76
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-11 15:06:08 +0100

    remove obsolete build scripts for chrome and firefox extensions

--- e2e/build.chrome.ts
+++
@@ -1,9 +0,0 @@
-import { test as setup } from "@playwright/test";
-import { build } from "wxt";
-
-setup("Build chrome extension", async ({}) => {
-  await build({
-    browser: "chrome",
-  });
-  console.log("Chrome extension build");
-});
--- e2e/build.firefox.ts
+++
@@ -1,9 +0,0 @@
-import { test as setup } from "@playwright/test";
-import { build } from "wxt";
-
-setup("Build firefox extension", async ({}) => {
-  await build({
-    browser: "firefox",
-  });
-  console.log("Firefox extension build");
-});
