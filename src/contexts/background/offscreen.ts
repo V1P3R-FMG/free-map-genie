13989e2b6b0742d12250ad9eb79be583426ffb8f
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-16 08:19:07 +0100

    remove global offscreenutils

--- src/contexts/background/offscreen.ts
+++ src/contexts/background/offscreen.ts
@@ -1,12 +1,37 @@
 import initOffscreen from "../offscreen.content/init";
 
+import type { PublicPath, Browser } from "wxt/browser";
+
+type PublicPathLike = PublicPath | (string & {});
+
+const create = async (
+  url: PublicPathLike,
+  reasons: Browser.offscreen.CreateParameters["reasons"],
+  justification: string
+) => {
+  const existingContexts = await browser.runtime.getContexts({
+    contextTypes: ["OFFSCREEN_DOCUMENT"],
+    documentUrls: [browser.runtime.getURL(url)],
+  });
+
+  // If an offscreen document already exists, we don't need to create another one
+  const exists = existingContexts.length > 0;
+  if (exists) return;
+
+  await browser.offscreen.createDocument({
+    url,
+    reasons,
+    justification,
+  });
+};
+
 export const createOffscreenDocument = async () => {
   if (import.meta.env.FIREFOX) {
     // Firefox doesn't support offscreen documents, so we use the background page instead
     await initOffscreen();
   } else {
     // For other browsers, we create an offscreen document
-    await OffscreenUtils.create(
+    await create(
       "/offscreen.html",
       ["IFRAME_SCRIPTING"],
       "Managing Iframes for Backend, Storage, etc."
