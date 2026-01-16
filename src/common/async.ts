86aa4b57138d0da11ddb8add01fddc9f0a2b7d95
MrFusiion <fuse-x-fuzionzz@outlook.be>
2026-01-16 17:01:36 +0100

    set default timeout for waituntil to 1 minute

--- src/common/async.ts
+++ src/common/async.ts
@@ -1,4 +1,4 @@
-import waitUntil from "async-wait-until";
+import _waitUntil from "async-wait-until";
 
 export const some = async (promises: Promise<boolean>[]): Promise<boolean> => {
   return new Promise<boolean>(async (resolve, reject) => {
@@ -19,6 +19,20 @@ export const some = async (promises: Promise<boolean>[]): Promise<boolean> => {
   });
 };
 
-export { waitUntil };
+export const waitUntil = (...args: Parameters<typeof _waitUntil>) => {
+  const [predicate, options, timeBetween] = args;
+
+  const optionsObject =
+    typeof options === "number" ? { timeout: options } : options ? options : {};
+
+  return _waitUntil(
+    predicate,
+    {
+      timeout: 60000,
+      ...optionsObject,
+    },
+    timeBetween
+  );
+};
 
 export default { some, waitUntil };
