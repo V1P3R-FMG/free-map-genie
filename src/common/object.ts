4e4f16beaebda4de9257d5ba8c844b3fdc5bb67c
MrFusiion <71613276+MrFusiion@users.noreply.github.com>
2026-01-20 02:32:00 +0100

    fix waitforproperty to allow it to be used across mutliple contexts at the same time without interfering with each other

--- src/common/object.ts
+++ src/common/object.ts
@@ -1,63 +1,93 @@
-const WaitForPropSymbol = Symbol("WaitForProp");
+const PropertyObserverSymbol = Symbol.for("PropertyObserver");
 
-interface TWithWaitForProp {
-  [WaitForPropSymbol]?: Set<(value: any) => void>;
-}
-
-export class WaitForPropertyTimeoutError<
+export class PropertyObserverTimeoutError<
   T extends object,
-  K extends keyof T
+  K extends keyof T,
 > extends Error {
-  constructor(public readonly target: T, public readonly property: K) {
+  constructor(
+    public readonly target: T,
+    public readonly property: K
+  ) {
     super(`Timeout waiting for property ${String(property)}`);
   }
 }
 
+type Listeners = Set<(value: any) => void>;
+
+export class PropertyObserver<T extends object> {
+  private readonly observers = new Map<PropertyKey, Listeners>();
+
+  private readonly target: T;
+
+  private constructor(target: T) {
+    this.target = target;
+  }
+
+  public static attach(target: {
+    [PropertyObserverSymbol]?: PropertyObserver<any>;
+  }) {
+    return (target[PropertyObserverSymbol] ??= new this(target));
+  }
+
+  public observe<K extends keyof T>(prop: K, cb: (value: T[K]) => void) {
+    if (!this.observers.has(prop)) {
+      if (prop in this.target) {
+        cb(this.target[prop]);
+        return;
+      } else {
+        this.attachObserver(prop);
+      }
+    }
+
+    this.observers.get(prop)!.add(cb);
+  }
+
+  private attachObserver<K extends keyof T>(prop: K) {
+    const listeners = new Set<(value: any) => void>();
+
+    this.observers.set(prop, listeners);
+
+    Object.defineProperty(this.target, prop, {
+      set: (value: any) => {
+        // Notify all waiters
+        listeners.forEach((cb) => cb(value));
+
+        // Remove listeners for prop
+        listeners.clear();
+        this.observers.delete(prop);
+
+        // Redefine the property to be a normal property
+        Object.defineProperty(this.target, prop, {
+          value,
+          writable: true,
+          enumerable: false,
+          configurable: true,
+        });
+      },
+      configurable: true,
+      enumerable: true,
+    });
+  }
+}
+
 export const waitForProperty = <T extends object, K extends keyof T>(
-  obj: T & TWithWaitForProp,
+  target: T,
   prop: K,
   timeout = 10000
 ): Promise<T[K]> => {
   return new Promise<T[K]>((resolve, reject) => {
-    const target = obj as TWithWaitForProp;
-
-    if (prop in obj) {
-      return resolve(obj[prop]);
-    }
-
+    let handle: number | null = null;
     if (timeout && timeout > 0) {
-      setTimeout(() => {
-        reject(new WaitForPropertyTimeoutError(obj, prop));
+      handle = window.setTimeout(() => {
+        reject(new PropertyObserverTimeoutError(target, prop));
       }, timeout);
     }
 
-    if (target[WaitForPropSymbol]) {
-      target[WaitForPropSymbol]!.add(resolve);
-      return;
-    }
+    const observer = PropertyObserver.attach(target);
 
-    target[WaitForPropSymbol] = new Set();
-    target[WaitForPropSymbol]!.add(resolve);
-
-    Object.defineProperty(obj, prop, {
-      set(value: any) {
-        // Notify all waiters
-        target[WaitForPropSymbol]?.forEach((waiter) => waiter(value));
-
-        // Redefine the property to be a normal property
-        Object.defineProperty(obj, prop, {
-          value,
-          writable: true,
-          enumerable: true,
-          configurable: true,
-        });
-
-        // Clean up
-        target[WaitForPropSymbol]?.clear();
-        delete target[WaitForPropSymbol];
-      },
-      configurable: true,
-      enumerable: true,
+    observer.observe(prop, (value) => {
+      handle && window.clearTimeout(handle);
+      resolve(value);
     });
   });
 };
