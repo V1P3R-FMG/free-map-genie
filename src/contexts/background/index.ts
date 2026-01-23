b1684e1f03c6376ad7c072b32bf2a29d8f807f3f
MrFusiion <71613276+MrFusiion@users.noreply.github.com>
2026-01-23 15:46:39 +0100

    add conditional check for rulesmanager.enable based on extensionsettings

--- src/contexts/background/index.ts
+++ src/contexts/background/index.ts
@@ -7,6 +7,7 @@ import testService from "@/services/test.service";
 import mapgenieService from "@/services/mapgenie.service";
 import backgroundService from "@/services/background.service";
 import { backgroundLoggerService } from "@/services/logger.service";
+import { ExtensionSettings } from "@/common/extension/settings";
 
 export default defineBackground(async () => {
   testService.provide();
@@ -16,7 +17,9 @@ export default defineBackground(async () => {
 
   const rulesManager = new Rules();
 
-  await rulesManager.enable();
+  if (await ExtensionSettings.enabled.getValue()) {
+    await rulesManager.enable();
+  }
   await createOffscreenDocument();
 
   logger.log("Hello background!", { id: browser.runtime.id });
